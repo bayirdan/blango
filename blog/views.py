9be901d83b61e94f9f5c303300f8d0e894718155
Burkay Bayır <burkay.bayir@artistanbul.io>
2024-03-13 09:10:22 +0000

    Finish crispy forms

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from blog.forms import CommentForm
 
 # Create your views here.
 
@@ -11,5 +13,22 @@ def index(request):
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
+    if request.user.is_active:
+      if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+          comment = comment_form.save(commit=False)
+          comment.content_object = post
+          comment.creator = request.user
+          comment.save()
+          return redirect(request.path_info)
+      else:
+        comment_form = CommentForm()
+    else:
+      comment_form = None
+
     post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {"post": post})
+    return render(
+        request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
+    )
